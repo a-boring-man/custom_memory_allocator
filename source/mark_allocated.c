4b455611f12b7fd93f1df71b628e6dbf3fc44fea
Jonathan Rinna <jonathan.rinna@gmail.com>
2023-12-04 13:34:59 +0100

    adding diagramme to explaine the memory layout befre and after allocating

--- source/mark_allocated.c
+++ source/mark_allocated.c
@@ -1,8 +1,48 @@
 #include "malloc.h"
 
 
+//								FREE BLOCK BEFORE
+////////////////////////////////////////////////////////////////////////////
+//S(S | T |                                                          |S(S //
+//I I | _ |                                                          |I I //
+//Z Z | L |                                                          |Z Z //
+//E E | I |                                                          |E E //
+//  _ | S |                                                          |  _ //
+//  T)| T |                                                          |  T)//
+////////////////////////////////////////////////////////////////////////////
+//									|
+//									|
+//									|
+//									|
+//									|
+//									V
+//								SPLITED_BLOCK
+////////////////////////////////////////////////////////////////////////////
+//S(S |R Z|                              |R Z|S(S |S(S | T |         |S(S //
+//I I |E O|                              |E O|I I |I I | _ |         |I I //
+//Z Z |D N|	P A Y L O A D                |D N|Z Z |Z Z | L |FREE ZONE|Z Z //
+//E E |  E|                              |  E|E E |E E | I |         |E E //
+//  _ |   |                              |   |  _ |  _ | S |         |  _ //
+//  T)|   |                              |   |  T)|  T)| T |         |  T)//
+////////////////////////////////////////////////////////////////////////////
+//									|
+//									OR
+//									|
+//									V
+//								WASTED_BLOCK
+////////////////////////////////////////////////////////////////////////////
+//S(S |R Z|                                                     |R Z|S(S |//
+//I I |E O|                                                     |E O|I I |//
+//Z Z |D N|	P A Y L O A D                                       |D N|Z Z |//
+//E E |  E|                                                     |  E|E E |//
+//  _ |   |                                                     |   |  _ |//
+//  T)|   |                                                     |   |  T)|//
+////////////////////////////////////////////////////////////////////////////
+
+
+
 /**
- * @brief the goql of this function is to mark a chunck of memory as allocated
+ * @brief the goal of this function is to mark a chunck of memory as allocated
  * 
  * @param block is the addres of a t_list in a free block
  * @param size_to_be_allocated is the size to be allocated taking into acount the padding if needed
@@ -41,5 +81,3 @@ void	*mark_block_as_allocated(t_list *block, size_t size_to_be_allocated, t_zone
 	*((size_t *)working_pointer) += 1;
 	return NULL;
 }
-
-// le but ici est de diviser un gros block free en un block plein et un block free si possible si block free alors le ratacher correcteemnt
