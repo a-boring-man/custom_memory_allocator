8d01283b6e98be2ca9c50902c7097b295a61fd53
Jonathan Rinna <jonathan.rinna@gmail.com>
2023-12-07 17:15:15 +0100

    i think first fit is done

--- source/find_free_block.c
+++ source/find_free_block.c
@@ -32,7 +32,7 @@ void	*first_fit(size_t size_to_be_alloc, t_zone *zone) {
 	{
 		list_head = list_head->next;
 	}
-	if (list_head == NULL || (list_head->next == zone->free && *((size_t *)(list_head) - 1) < padded(size_to_be_alloc) + MINIMUM_ALLOCATED_BLOCK_SIZE)) {
+	if (list_head == NULL || (list_head->next == zone->free && *((size_t *)(list_head) - 1) < padded(size_to_be_alloc) + MINIMUM_ALLOCATED_BLOCK_SIZE)) { // no block was found need to create a new page
 		t_memory_pointer	new_page;
 
 		new_page.as_void = create_page(zone, size_to_be_alloc); 
@@ -40,6 +40,9 @@ void	*first_fit(size_t size_to_be_alloc, t_zone *zone) {
 		add_block_to_t_list((t_list *)(new_page.as_sizeT + 1), (t_list **)(&(zone->page)));
 		add_block_to_t_list((t_list *)(new_page.as_char + PAGE_START_OVERHEAD + sizeof(size_t)), (t_list **)(&(zone->free)));
 		return (mark_block_as_allocated((t_list *)(new_page.as_char + PAGE_START_OVERHEAD + sizeof(size_t)), padded(size_to_be_alloc), zone));
+	}
+	else { // if a block was found
+		return (mark_block_as_allocated(list_head, padded(size_to_be_alloc), zone));
 	}
 	// if end do the same as is free list was null and return the new free block created
 	// else return the block
