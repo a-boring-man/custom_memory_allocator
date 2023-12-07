c2f2820f1ecb1f4694732dee119f874da5a7de3c
Jonathan Rinna <jonathan.rinna@gmail.com>
2023-12-07 16:25:03 +0100

    adding the while loop of the first fit function

--- source/find_free_block.c
+++ source/find_free_block.c
@@ -25,6 +25,13 @@ void	*next_fit(size_t size_to_be_alloc, t_zone *zone) {
 void	*first_fit(size_t size_to_be_alloc, t_zone *zone) {
 	t_list	*list_head = zone->free;
 
+	// whyle free list is not empty and free block size <= size_to_be_allocated continue
+	while (list_head != NULL \
+		&& list_head->next != zone->free \
+		&& *((size_t *)(list_head) - 1) < padded(size_to_be_alloc) + MINIMUM_ALLOCATED_BLOCK_SIZE)
+	{
+		list_head = list_head->next;
+	}
 	if (list_head == NULL) { // empty free list
 		t_memory_pointer	new_page;
 		new_page.as_void = create_page(zone, size_to_be_alloc); 
@@ -32,10 +39,7 @@ void	*first_fit(size_t size_to_be_alloc, t_zone *zone) {
 		add_block_to_t_list((t_list *)(new_page.as_sizeT + 1), (t_list **)(&(zone->page)));
 		add_block_to_t_list((t_list *)(new_page.as_char + PAGE_START_OVERHEAD + sizeof(size_t)), (t_list **)(&(zone->free)));
 	}
-	while {list_head != NULL \
-		&& list_head->next != list_head \
-		&& *((size_t *)(list_head) - 1) <}
-	// whyle free list is not empty and free block size <= size_to_be_allocated continue
+	
 	// if end do the same as is free list was null and return the new free block created
 	// else return the block
 	return NULL;
