89f81e2dfe05d948d26b51d8810c2608d0de89d3
Jonathan Rinna <jonathan.rinna@gmail.com>
2023-12-08 13:35:23 +0100

    cleaning the header file

--- include/malloc.h
+++ include/malloc.h
@@ -1,14 +1,12 @@
 #ifndef MALLOC_H
 #define MALLOC_H
 
+// ------------dependency----------
+
 #include <stdio.h>
 #include <unistd.h>
 
-# ifdef RED_ZONE_DEBUG
-	#define RED_ZONE_SIZE 2 * sizeof(size_t)
-# else
-	#define RED_ZONE_SIZE 0
-# endif
+// ------------struct-------------
 
 typedef struct s_list {
 	struct s_list	*next;
@@ -29,6 +27,13 @@ typedef union u_memory_pointer {
 	char	*as_char;
 }	t_memory_pointer;
 
+// -------------usefull define--------------
+
+# ifdef RED_ZONE_DEBUG
+	#define RED_ZONE_SIZE 2 * sizeof(size_t)
+# else
+	#define RED_ZONE_SIZE 0
+# endif
 # define MAX_SIZET (~(size_t)0)
 # define MINIMUM_FREE_BLOCK_SIZE 2 * sizeof(size_t) + sizeof(t_list)
 # define MINIMUM_ALLOCATED_BLOCK_SIZE 2 * sizeof(size_t) + 2 * RED_ZONE_SIZE
@@ -37,33 +42,47 @@ typedef union u_memory_pointer {
 # define MINIMUM_PAGE_SIZE PAGE_OVERHEAD + 2 * RED_ZONE_SIZE
 # define RED_ZONE_COLOR 0xCC
 
+// -------------global allocation variable-------------
+
 extern t_zone grimoire[11];
+
+// -------------mandatory function---------------
 
 void	free(void *ptr);
 void	*malloc(size_t size);
 void	*realloc(void *ptr, size_t size);
 void	show_alloc_mem();
+
+// -------------bonus function-------------
+
 void	show_alloc_mem_ex();
-
-t_zone	*choose_the_right_page(size_t size);
-void	*create_page(t_zone *zone, size_t size);
-
-size_t	ceilling_unsigned(double x);
-size_t	padded(size_t size);
-void	add_block_to_t_list(t_list *new_block, t_list **list_head);
-void	add_block_to_t_list_address_ordered(t_list *new_block, t_list **list_head);
-void	add_block_to_t_list_first(t_list *new_block, t_list **list_head);
 void	poison_block(void *block, size_t size, char poison);
 void	*red_zone(void *block, size_t size_allocated);
-
-void	format_new_page(void *new_page, size_t page_size);
-void	format_free_space(void *new_page, size_t free_block_size);
-size_t	determine_page_size(t_zone *zone, size_t size_to_be_malloc);
-void	remove_block_from_t_list(t_list *block, t_list **head);
-void	*mark_block_as_allocated(t_list *block, size_t size_to_be_allocated, t_zone * zone);
-
+t_zone	*choose_the_right_page(size_t size);
 void	*best_fit(size_t size_to_be_alloc, t_zone *zone);
 void	*next_fit(size_t size_to_be_alloc, t_zone *zone);
 void	*first_fit(size_t size_to_be_alloc, t_zone *zone);
+
+// -------------memory function----------------
+
+void	*create_page(t_zone *zone, size_t size);
+void	format_new_page(void *new_page, size_t page_size);
+void	format_free_space(void *new_page, size_t free_block_size);
+void	*mark_block_as_allocated(t_list *block, size_t size_to_be_allocated, t_zone * zone);
+
+// -------------t_list function----------------
+
+void	add_block_to_t_list(t_list *new_block, t_list **list_head);
+void	add_block_to_t_list_address_ordered(t_list *new_block, t_list **list_head);
+void	add_block_to_t_list_first(t_list *new_block, t_list **list_head);
+void	remove_block_from_t_list(t_list *block, t_list **head);
+
+// -------------helper function-----------------
+
+size_t	ceilling_unsigned(double x);
+size_t	padded(size_t size);
+size_t	determine_page_size(t_zone *zone, size_t size_to_be_malloc);
+
+// -----------------------------------------
 
 #endif
