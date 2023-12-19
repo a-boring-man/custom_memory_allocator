fecd0148a0f4f0255e61a2857f7715c83058c876
Jonathan Rinna <jonathan.rinna@gmail.com>
2023-12-19 14:23:17 +0100

    mayby fix the bug

--- main.c
+++ main.c
@@ -21,16 +21,16 @@ int main() {
     test2[0] = 8942;
     test2[1] = 23;
     show_alloc_mem();
-    //debug_hexa(test - 5 - RED_ZONE_SIZE, 40);
+    debug_hexa(test - 5 - RED_ZONE_SIZE, 40);
     ft_printf("first_free\n");
+    show_alloc_mem();
     free(test);
-    show_alloc_mem();
-    //debug_hexa(test - 5 - RED_ZONE_SIZE, 40);
+    debug_hexa(test - 5 - RED_ZONE_SIZE, 40);
     ft_printf("second_free\n");
-    //free(test1);
+    free(test1);
     show_alloc_mem();
-    //debug_hexa(test - 5 - RED_ZONE_SIZE, 40);
-    //free(test2);
+    debug_hexa(test - 5 - RED_ZONE_SIZE, 40);
+    free(test2);
     show_alloc_mem();
-    //debug_hexa(test - 5 - RED_ZONE_SIZE, 40);
+    debug_hexa(test - 5 - RED_ZONE_SIZE, 40);
 }
--- source/core/free.c
+++ source/core/free.c
@@ -8,14 +8,14 @@ void	free(void *ptr) {
 		return;
 	}
 
-	block_size = *((size_t *)ptr - (1 + RED_ZONE_SIZE)) & -2;
+	block_size = *(size_t *)((char *)ptr - (sizeof(size_t) + RED_ZONE_SIZE)) & -2;
 	data_size = block_size - MINIMUM_ALLOCATED_BLOCK_SIZE;
 	ft_printf("wanting to free the block at : -%p- of size : -%u- and of true size : -%u-\n", ptr, block_size, data_size);
 	t_zone	*zone = choose_the_right_page(data_size);
 	
 	
 	mark_block_as_free(ptr, zone);
-	coalescing(ptr, zone);
+	//coalescing(ptr, zone);
 	
 	(void)ptr;
 	int pff = write(1, "coucou\n", 7);
