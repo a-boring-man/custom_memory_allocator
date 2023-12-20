d8e9265a49547464e92e06683f156d11229d4da5
Jonathan Rinna <jonathan.rinna@gmail.com>
2023-12-20 17:24:50 +0100

    adding the detect page full of small block features

--- source/core/free.c
+++ source/core/free.c
@@ -1,14 +1,26 @@
 #include "malloc.h"
 
+static size_t check_if_only_contain_free_block(void *first_free_block) { // the addres is the begginning of the size of the freeblock
+	t_memory_pointer	working_pointer;
+	
+	working_pointer.as_void = first_free_block;
+	while (!(*working_pointer.as_sizeT & 1)) { // stop if a block is allocated
+		working_pointer.as_char += *working_pointer.as_sizeT; // jump to the next block
+	}
+	return (*working_pointer.as_sizeT == 1); // return true if we stop because we reach the marker of end of page
+}
+
 static int check_if_page_is_empty(void *page) {
 	t_memory_pointer	working_pointer;
 	size_t	page_size;
+	size_t	has_only_free_block = 0;
 
 	working_pointer.as_void = page;
 	working_pointer.as_sizeT -= 1; // move the pointer to the beginning of the page
 	page_size = *working_pointer.as_sizeT; // store page size
 	working_pointer.as_char += PAGE_START_OVERHEAD;
-	return (page_size == *working_pointer.as_sizeT + PAGE_OVERHEAD);
+	has_only_free_block = check_if_only_contain_free_block(working_pointer.as_void);
+	return (!(*working_pointer.as_sizeT & 1) && (page_size == *working_pointer.as_sizeT & -2 + PAGE_OVERHEAD || free_block_sum + PAGE_OVERHEAD == page_size)); // return true if the first block is not malloc AND either the hol page is a free block or composed of free block only
 }
 
 static void check_for_unmap_page() {
